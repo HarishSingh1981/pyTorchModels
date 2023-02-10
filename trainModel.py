1ab3ee1216ca3df5cd0f43bc9a456ac146b48888
HarishSingh1981 <HarishSingh1981@users.noreply.github.com>
2023-02-10 13:35:53 +0530

    indentation fix

--- trainModel.py
+++ trainModel.py
@@ -9,22 +9,21 @@ def train_model(dataloader,network,lossfn,optimizer):
     correct = 0
     total = 0
     batches = 0
-    for batch_idx, (inputs, targets) in enumerate(dataloader):
-        inputs = list(inputs.values())
-        inputs = inputs[0]
-        inputs, targets = inputs.to(device), targets.to(device)
-        optimizer.zero_grad()
-        #outputs = network(np.transpose(inputs,(0,3,1,2)))
-		outputs = network(inputs.permute(0,3,1,2))
-        loss = lossfn(outputs, targets)
-        loss.backward()
-        optimizer.step()
-        batches +=1
 
-        train_loss += loss.item()
-        _, predicted = outputs.max(1)
-        total += targets.size(0)
-        correct += predicted.eq(targets).sum().item()
+    for batch_idx, (inputs, targets) in enumerate(dataloader):
+		inputs = list(inputs.values())
+		inputs = inputs[0]
+		inputs, targets = inputs.to(device), targets.to(device)
+		optimizer.zero_grad()
+		outputs = network(inputs.permute(0,3,1,2))
+		loss = lossfn(outputs, targets)
+		loss.backward()
+		optimizer.step()
+		batches +=1
+		train_loss += loss.item()
+		_, predicted = outputs.max(1)
+		total += targets.size(0)
+		correct += predicted.eq(targets).sum().item()
 
     print(batches, len(dataloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                      % (train_loss/(batches), 100.*correct/total, correct, total))
