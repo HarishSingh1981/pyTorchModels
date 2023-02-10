286f746d8253363f0e879bcb99ac26dd6f309a65
HarishSingh1981 <HarishSingh1981@users.noreply.github.com>
2023-02-10 12:44:23 +0530

    All parameters made non-default

--- dataloader.py
+++ dataloader.py
@@ -7,7 +7,7 @@ use_cuda = 'cuda' if torch.cuda.is_available() else 'cpu'
 kwargs_train = dict(num_workers= 4, pin_memory= True if use_cuda else False,shuffle=True,batch_size=batch_size)
 
 
-def getDataLoader(dataset,isTrain=True,transform,batchSize,isShuffle=True,workers=4,needDownload=True):
+def getDataLoader(dataset,isTrain,transform,batchSize,isShuffle,workers,needDownload):
 	kwargs = dict(num_workers= workers, pin_memory= True if use_cuda else False,shuffle=isShuffle,batch_size=batchSize)
 	data_set = dataset(root='./data', train=isTrain,
                                         download=needDownload, transform=transform)
