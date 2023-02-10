c4c51425175cf6e19822d709a5527e74d4647c06
HarishSingh1981 <HarishSingh1981@users.noreply.github.com>
2023-02-10 12:46:22 +0530

    minor error fix

--- dataloader.py
+++ dataloader.py
@@ -4,9 +4,6 @@ import torchvision.transforms as transforms
 
 use_cuda = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-kwargs_train = dict(num_workers= 4, pin_memory= True if use_cuda else False,shuffle=True,batch_size=batch_size)
-
-
 def getDataLoader(dataset,isTrain,transform,batchSize,isShuffle,workers,needDownload):
 	kwargs = dict(num_workers= workers, pin_memory= True if use_cuda else False,shuffle=isShuffle,batch_size=batchSize)
 	data_set = dataset(root='./data', train=isTrain,
