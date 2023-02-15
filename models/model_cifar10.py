84874d4661967a3ffb8e22213e62cbed546cc513
HarishSingh1981 <HarishSingh1981@users.noreply.github.com>
2023-02-15 15:00:35 +0530

    Update model_cifar10.py

--- models/model_cifar10.py
+++ models/model_cifar10.py
@@ -2,6 +2,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchsummary import summary
 
+class DepthwiseSeparable(nn.Module):
+  def __init__(self, in_ch, out_ch, stride=1):
+    super(DepthwiseSeparable, self).__init__()
+    self.in_chan = in_ch
+    self.out_chan = out_ch
+
+    self.depthwise = nn.Sequential(
+          nn.Conv2d(in_channels=self.in_chan, out_channels=self.in_chan, kernel_size=(3, 3), padding=1, stride=stride, groups=self.in_chan, bias=False),
+          #pointwise
+          nn.Conv2d(in_channels=self.in_chan, out_channels=self.out_chan, kernel_size=(1,1)))
+
+  def forward(self, x):
+    x = self.depthwise(x)
+    return x
 
 class Net_Cifar10(nn.Module):
     def __init__(self):
@@ -11,51 +25,76 @@ class Net_Cifar10(nn.Module):
         nout = (n_in + 2*p-k)/s + 1
         rf_out = rf_in + (k-1)*j_in
         '''
-        #input 3x32x32 -? OUtput 6x28x28? RF 5
-        self.c1 = nn.Sequential(nn.Conv2d(in_channels=3,out_channels=6,kernel_size=5),
-                                      nn.BatchNorm2d(6),
-                                      nn.ReLU())
-                                      #nn.Dropout2d(0.5))
-        #input 6x28x28 -? OUtput 6x20x20? RF 13
-        self.transitionBlock1 = nn.Sequential(nn.Conv2d(in_channels=6, out_channels=6, kernel_size=5,dilation=2),
-                                      nn.BatchNorm2d(6),
-                                      nn.ReLU())
-        #input 6x20x20 -? OUtput 16x20x20? RF 17
-        self.c2 = nn.Sequential(nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5,padding=2),
-                                      nn.BatchNorm2d(12),
-                                      nn.ReLU())
-                                      #nn.Dropout2d(0.5))
-        #input 12x20x20 -? OUtput 12x12x12? RF 33
-        #self.transitionBlock1 = nn.MaxPool2d(2,2)
-        self.transitionBlock2 = nn.Sequential(nn.Conv2d(in_channels=12, out_channels=12, kernel_size=5,dilation=4,padding=4),
-                                      nn.BatchNorm2d(12),
-                                      nn.ReLU())
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),  # Input: 32x32x3 | Output: 32x32x32 | RF: 3x3
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Dropout(drop),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, bias=False),   # Input: 32x32x32 | Output: 32x32x32 | RF: 5x5
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Dropout(drop)
+        )
 
-        #input 12x12x12 -? OUtput 16x8x8? RF 37
-        self.c3 = nn.Sequential(nn.Conv2d(12,16,5),
-                                      nn.BatchNorm2d(16),
-                                      nn.ReLU())
-                                      #nn.Dropout2d(0.5))
+        self.transblock1 =  nn.Sequential(
+            #Stride 2 conv
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, stride=2, bias=False),  # Input: 32x32x32 | Output: 16x16x32 | RF: 7x7
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Dropout(drop)
+        )
 
-        #input 16x8x8 -? OUtput 10x4x4? RF 45
-        self.c4 = nn.Sequential(nn.Conv2d(16,16,5,groups=16),
-                                nn.Conv2d(16,10,1),
-                                nn.BatchNorm2d(10),
-                                nn.ReLU())
-                                #nn.Dropout2d(0.5))               
-        self.avgPoolblk = nn.AvgPool2d(4,4)
-        self.fc = nn.Linear(12,10)
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),   # Input: 16x16x32 | Output: 16x16x64 | RF: 11x11
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout(drop),  
+        ) 
+
+        self.transblock2 =  nn.Sequential(
+            #Stride 2 conv
+            DepthwiseSeparable(64,64,2), # Input: 16x16x64 | Output: 8x8x64 | RF: 15x15
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout(drop),
+        )
+
+        self.convblock3 = nn.Sequential(
+            DepthwiseSeparable(64, 128), # Input: 8x8x64 | Output: 8x8x128 | RF: 23x23
+            # nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Dropout(drop),  
+            nn.Conv2d(in_channels=128, out_channels=32, kernel_size=(1, 1), bias=False), # Input: 8x8x128 | Output: 8x8x32 | RF: 23x23
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Dropout(drop), 
+
+        ) 
+
+        self.transblock3 =  nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, dilation=2, bias=False), # Input: 8x8x128 | Output: 6x6x32 | RF: 39x39
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, dilation=2, bias=False), # Input: 6x6x32 | Output: 4x4x32 | RF: 55x55
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Dropout(drop),
+        )
+
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), padding=1, bias=False), # Input: 4x4x32 | Output: 4x4x32 | RF: 63x63
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=10, kernel_size=(3, 3), padding=1, bias=False), # Input: 4x4x32 | Output: 4x4x10 | RF: 71x71
+
+        ) 
+
+        self.gap = nn.AvgPool2d(4)
     def forward(self, x):
       #maxpool must be used at least after 2 convolution and sud be as far as possible from last layer
         #x = x.to('cuda')
-        x = self.c1(x)
-        x = self.transitionBlock1(x)
-        x = self.c2(x)
-        x = self.transitionBlock2(x)
-        x = self.c3(x)
-        x = self.c4(x)
-        x = self.avgPoolblk(x)
-    #    print(f'shape of x after GAP is {x.shape}')
+        x =  self.transblock1(self.convblock1(x))
+        x =  self.transblock2(self.convblock2(x))
+        x =  self.transblock3(self.convblock3(x))
+        x =  self.convblock4(x)
+        x =  self.gap(x)
         x = x.view(-1, 10)
-       # x = self.fc(x)
-        return x
+        return F.log_softmax(x, dim=-1)
